80165bb4c86e60374b602585124dc71483fec703
Juan Cappi <jmcappi@us.ibm.com>
2013-10-08 10:24:43 -0700

    segundo commit

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,9 +1,7 @@
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println();
-	System.out.println();
-	System.out.println();
-	System.out.println();
+	System.out.println("bla");
+	System.out.println("bla2");
 }
 }
